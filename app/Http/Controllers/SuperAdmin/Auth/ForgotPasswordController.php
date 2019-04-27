262d8cde6152aec26706a23892d2d2302b601647
Daud <daudonmail@gmail.com>
2019-04-27 12:04:31 +0100

    Adding Cors Header

--- app/Http/Controllers/SuperAdmin/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/SuperAdmin/Auth/ForgotPasswordController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\SuperAdmin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
