0d8b109eef21ab9e939752f32318233f5391fcd8
kevchike <kevchike@gmail.com>
2019-04-27 23:44:01 +0100

    Update for Heroku

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Kid Stories'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://api-kidstories.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
