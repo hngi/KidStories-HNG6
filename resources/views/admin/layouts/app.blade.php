baca56ca57479f25a8e27cf719d698b8d72fddb4
kevchike <kevchike@gmail.com>
2019-04-27 23:34:30 +0100

    Load assets over https

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,14 +10,14 @@
 
     <title>{{ config('app.name', 'Argon Dashboard') }}</title>
     <!-- Favicon -->
-    <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+    <link href="{{ secure_asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <!-- Icons -->
-    <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="{{ secure_asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="{{ secure_asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
     <!-- Argon CSS -->
-    <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
+    <link type="text/css" href="{{ secure_asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
 </head>
 
 <body class="{{ $class ?? '' }}">
@@ -37,13 +37,13 @@
     @include('admin.layouts.footers.guest')
     @endif
 
-    <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
-    <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ secure_asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="{{ secure_asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('js')
 
     <!-- Argon JS -->
-    <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script>
+    <script src="{{ secure_asset('argon') }}/js/argon.js?v=1.0.0"></script>
 </body>
 
 </html>
