0a7bd5bc677cf15a33b199c5bb74b689f02650e1
tuck183 <okorie.tochi@gmail.com>
2019-04-29 17:59:43 +0100

    landing page

---
+++ index.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Kids Stories | Homepage</title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/reset.css" rel="stylesheet">
+<link href="css/tstyle.css" rel="stylesheet">
+<!-- Responsive -->
+<link href="css/tresponsive.css" rel="stylesheet"> 
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+
+<body>
+<div class="page-wrapper">
+  	
+    <!-- Main Header-->
+    <header class="main-header header-style-one">
+    	
+        <!--Header-Upper-->
+        <div class="header-upper">
+        	<div class="auto-container">
+            	<div class="clearfix">
+                	<!-- start of Logo -->
+                	<div class="pull-left logo-outer">
+                    	<div class="logo"><a href="#"><img src="images/logo.png" alt="" title="" width="105px" height="auto"></a></div>
+                    </div>
+                   	
+					<div class="pull-right upper-right clearfix">
+						<div class="nav-outer clearfix">
+						
+							<!-- Main Menu -->
+							<nav class="main-menu navbar-expand-md">
+								<div class="navbar-header">
+									<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								</div>
+
+								<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+									<ul class="navigation clearfix">
+										<li class="current"><a href="#">Home</a>
+										</li>
+										<li class=""><a href="#">Browse Stories</a>
+										</li>
+										<li><a href="#">Categories</a></li>
+										<li class=""><a href="#">My Stories</a>
+										</li>
+										<li><a href="#">About Us</a></li>
+									</ul>
+								</div>
+								
+							</nav>
+							
+							<!-- Main Menu End-->
+							<div class="outer-box">
+								<!--Search Box-->
+								<div class="search-box-outer">
+									<div class="dropdown">
+										<button class="search-box-btn dropdown-toggle" type="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-search"></span></button>
+										<ul class="dropdown-menu pull-right search-panel" aria-labelledby="dropdownMenu3">
+											<li class="panel-outer">
+												<div class="form-container">
+													<form method="post" action="#">
+														<div class="form-group">
+															<input type="search" name="field-name" value="" placeholder="Search Here" required>
+															<button type="submit" class="search-btn"><span class="fa fa-search"></span></button>
+														</div>
+													</form>
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<!--Language-->
+								<div class="language dropdown"><a class="btn btn-default dropdown-toggle" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" href="#"><span class="icon circle-icons fa fa-user"></span>  Account <span class="icon fa fa-caret-down"></span> </a>
+									<ul class="dropdown-menu style-one" aria-labelledby="dropdownMenu2">
+										<li><a href="#">Login</a></li>
+									</ul>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+					
+                </div>
+            </div>
+        </div>
+        <!--End Header Upper-->
+
+    </header>
+    <!--End Main Header -->
+    
+    <!--Form Back Drop-->
+    <div class="form-back-drop"></div>
+        
+    <!--Main Banner-->
+    <section class="banner-section-three">
+    	<div class="auto-container">
+			<div class="row clearfix">
+            	
+                <!--Content Column-->
+                <div class="content-column col-lg-6 col-md-12 col-sm-12">
+                	<div class="inner-column wow slideInLeft">
+                    	<h3>Get personalized bedtime stories<br> tailored just for your child</h3>
+                        <div class="text">Read free bedtime stories, fairy tales, poems and short <br> stories for kids</div>
+                        <a href="#" class="theme-btn btn-style-two">Start Reading <span class="icon flaticon-next-3"></span></a>
+                    </div>
+                </div>
+                <!--Image Column-->
+                <div class="image-column col-lg-6 col-md-12 col-sm-12">
+                	<div class="image paroller" data-paroller-factor="0.15" data-paroller-factor-lg="0.15" data-paroller-factor-md="0.10" data-paroller-factor-sm="0.10" data-paroller-type="foreground" data-paroller-direction="horizontal">
+                    	<img src="images/resources/frame.png" alt="" />
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!--End Banner Section -->
+    
+	<!-- Services Section -->
+	<section class="services-section">
+		<div class="auto-container">
+			<!-- Sec Title -->
+			<div class="sec-title centered">
+				<h3>Why Kids Stories?</h3>
+			</div>
+			
+			<div class="row clearfix">
+				
+				<!-- Services Block -->
+				<div class="services-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="icon-box">
+							<img src="images/icons/people.png" alt="" title="" width="100px" height="100px"></a>
+						</div>
+						<h3>The Best Stories</h3>
+						<div class="text">Efficiently unleash cross-media information without.</div>
+					</div>
+				</div>
+				
+				<!-- Services Block -->
+				<div class="services-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+						<div class="icon-box">
+							<img src="images/icons/chat.png" alt="" title="" width="100px" height="100px"></a>
+						</div>
+						<h3>Huge Story Library</h3>
+						<div class="text">Efficiently unleash cross-media information without.</div>
+					</div>
+				</div>
+				
+				<!-- Services Block -->
+				<div class="services-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="600ms" data-wow-duration="1500ms">
+						<div class="icon-box">
+							<img src="images/icons/support.png" alt="" title="" width="100px" height="100px"></a>
+						</div>
+						<h3>Great Community</h3>
+						<div class="text">Efficiently unleash cross-media information without.</div>
+					</div>
+				</div>
+				
+				<!-- Services Block -->
+				<div class="services-block col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="900ms" data-wow-duration="1500ms">
+						<div class="icon-box">
+							<img src="images/icons/bank.png" alt="" title="" width="100px" height="100px"></a>
+						</div>
+						<h3>Really Affordable</h3>
+						<div class="text">Efficiently unleash cross-media information without.</div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+	</section>
+	<!-- End Services Section -->
+	
+	<!--  Section -->
+    <section class="main-banner">
+    	<div class="bg-round-layer"></div>
+        
+    	<div class="auto-container">
+			<div class="row clearfix">
+            	
+                <!--Content Column-->
+                <div class="content-column col-lg-8 col-md-12 col-sm-12">
+                    <div class="inner-column">
+                        <p class="text primary-color bold">WHAT IT DOES </p>
+                        <h4>An Enhanced Sleeping Experience</h4>
+                        <div class="text">The Kids Stories app is your go to app for free bedtime stories, fairy tales, poems and short stories for kids. Get in there and start reading!
+                        </div>
+                        <a href="#" class="primary-color underline play-btn">
+                                    <span class="icon flaticon-play-button-1"></span> watch how it works!
+                                </a>
+                    </div>
+                </div>
+                <!--Image Column-->
+                <div class="image-column col-lg-4 col-md-12 col-sm-12">
+                	<img src="images/resources/gp23.png" alt="" />
+                </div>
+                
+            </div>
+        </div>
+    </section>
+	<!-- End Section -->
+
+
+
+	<!-- Services Section -->
+	<section class="services-section bg-1">
+		<div class="auto-container">
+			<!-- Sec Title -->
+			<div class="sec-title centered">
+				<h3>Why use Kids Stories?</h3>
+				<div class="text col-md-6 col-sm-12 offset-3 m-t-50">The Kids Stories app is your go to app for free bedtime stories, fairy tales, poems and short stories for kids. Get in there and start reading!</p>
+			</div>
+			
+			<div class="row clearfix">
+				
+				<!-- Stats -->
+				<div class="services-block col-lg-4 col-md-4 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+
+						<h3>100+</h3>
+						<div class="text">Stories</div>
+					</div>
+				</div>
+				
+				<!-- Stats -->
+				<div class="services-block col-lg-4 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+						<h3>100+</h3>
+						<div class="text">Active Users</div>
+					</div>
+				</div>
+				
+				<!-- Stats -->
+				<div class="services-block col-lg-4 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="600ms" data-wow-duration="1500ms">
+						<h3>96+</h3>
+						<div class="text">Kids Improved</div>
+					</div>
+				</div>
+				
+				
+			</div>
+			
+		</div>
+	</section>
+	<!-- End Services Section -->
+
+	<!-- Faq Section -->
+	<section class="faq-section">
+		<div class="auto-container">
+			
+			<!-- Sec Title -->
+			<div class="sec-title centered">
+				<h4>What Our Users Say</h4>
+			</div>
+			
+			<div class="faq-carousel owl-carousel owl-theme text-left">
+				
+				<!-- Faq Block -->
+				<div class="faq-block text-left">
+					<div class="authors-box inner-box">
+						<div class="col-md-12">
+
+						<div class="pull-left">
+							<!-- Author Box -->
+							<div class="author-box">
+								<div class="author-inner">
+									<div class="image">
+										<img src="images/resources/author-3.jpg" alt="" />
+									</div>
+									<div class="author-name">Helena Kamil</div>
+								</div>
+							</div>
+						</div>
+						<div class="pull-right">
+							<!-- Social Nav -->
+							<div class="social-icons">
+								<a href="#"><span class="fa fa-twitter"></span></a>
+							</div>
+						</div>
+
+					</div><!-- end -->
+
+						<div class="col-md-12 p-t-70">
+						<div class="text text-left">
+							<p>Completely synergize resource via premier niche markets. customer service with robust ideas.</p>
+						</div>
+					</div><!-- end -->
+					</div>
+				</div>
+				
+				
+				<!-- Faq Block -->
+				<div class="faq-block text-left">
+					<div class="authors-box inner-box">
+						<div class="col-md-12">
+
+						<div class="pull-left">
+							<!-- Author Box -->
+							<div class="author-box">
+								<div class="author-inner">
+									<div class="image">
+										<img src="images/resources/author-3.jpg" alt="" />
+									</div>
+									<div class="author-name">Helena Kamil</div>
+								</div>
+							</div>
+						</div>
+						<div class="pull-right">
+							<!-- Social Nav -->
+							<div class="social-icons">
+								<a href="#"><span class="fa fa-twitter"></span></a>
+							</div>
+						</div>
+
+					</div><!-- end -->
+
+						<div class="col-md-12 p-t-70">
+						<div class="text text-left">
+							<p>Completely synergize resource via premier niche markets. customer service with robust ideas.</p>
+						</div>
+					</div><!-- end -->
+					</div>
+				</div>
+				
+				
+				<!-- Faq Block -->
+				<div class="faq-block text-left">
+					<div class="authors-box inner-box">
+						<div class="col-md-12">
+
+						<div class="pull-left">
+							<!-- Author Box -->
+							<div class="author-box">
+								<div class="author-inner">
+									<div class="image">
+										<img src="images/resources/author-3.jpg" alt="" />
+									</div>
+									<div class="author-name">Helena Kamil</div>
+								</div>
+							</div>
+						</div>
+						<div class="pull-right">
+							<!-- Social Nav -->
+							<div class="social-icons">
+								<a href="#"><span class="fa fa-twitter"></span></a>
+							</div>
+						</div>
+
+					</div><!-- end -->
+
+						<div class="col-md-12 p-t-70">
+						<div class="text text-left">
+							<p>Completely synergize resource via premier niche markets. customer service with robust ideas.</p>
+						</div>
+					</div><!-- end -->
+					</div>
+				</div>
+
+				
+				<!-- Faq Block -->
+				<div class="faq-block text-left">
+					<div class="authors-box inner-box">
+						<div class="col-md-12">
+
+						<div class="pull-left">
+							<!-- Author Box -->
+							<div class="author-box">
+								<div class="author-inner">
+									<div class="image">
+										<img src="images/resources/author-3.jpg" alt="" />
+									</div>
+									<div class="author-name">Helena Kamil</div>
+								</div>
+							</div>
+						</div>
+						<div class="pull-right">
+							<!-- Social Nav -->
+							<div class="social-icons">
+								<a href="#"><span class="fa fa-twitter"></span></a>
+							</div>
+						</div>
+
+					</div><!-- end -->
+
+						<div class="col-md-12 p-t-70">
+						<div class="text text-left">
+							<p>Completely synergize resource via premier niche markets. customer service with robust ideas.</p>
+						</div>
+					</div><!-- end -->
+					</div>
+				</div>
+
+				<!-- end -->
+				
+			</div>
+			
+		</div>
+	</section>
+	<!-- End Faq Section -->
+
+
+	<!-- App Section -->
+    <section class="main-banner">
+        
+    	<div class="auto-container">
+			<div class="row clearfix">
+
+                <!--Image Column-->
+                <div class=" col-lg-4 col-md-12 col-sm-12">
+                	<img src="images/resources/gp23.png" alt="" />
+                </div>
+
+            	
+                <!--Content Column-->
+                <div class="content-column col-lg-8 col-md-12 col-sm-12">
+                    <div class="inner-column">
+                        <h4>Get up close with your child</h4>
+                        <div class="text">The Kids Stories app is your go to app for free bedtime stories, fairy tales, poems and short stories for kids. Get in there and start reading!
+                        </div>
+					<div class="buttons-box">
+				<a href="#" class="theme-btn wow slideInLeft" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="images/icons/apple.png" alt="" /></a>
+				<a href="#" class="theme-btn wow slideInRight" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="images/icons/playstore.png" alt="" /></a>
+			</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+	<!-- End App Section -->
+
+
+
+	
+</div>
+<!--End pagewrapper-->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+
+<!-- end -->
+<script src="tjs/jquery.js"></script>
+<script src="tjs/popper.min.js"></script>
+<script src="tjs/bootstrap.min.js"></script>
+<script src="tjs/jquery-ui.js"></script>
+<script src="tjs/owl.js"></script>
+<script src="tjs/appear.js"></script>
+<script src="tjs/wow.js"></script>
+<script src="tjs/paroller.js"></script>
+<script src="tjs/script.js"></script>
+
+</body>
+</html>
