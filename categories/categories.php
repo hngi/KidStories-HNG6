7ae4218bf8a3e702a254c13f4092bf8a51b3d97f
Ebuka Abraham <buka4rill@gmail.com>
2019-04-29 21:27:14 +0100

    buka4rill categories FE

---
+++ categories/categories.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Google Fonts API -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <title> Kids Stories | Categories </title>
+</head>
+<body>
+    <!-- Header goes here -->
+
+
+    <!-- Showcase -->
+    <section class="top-container">
+            <header class="showcase">
+                <h1> Categories </h1>
+            </header>
+    </section>
+        <!-- Navigation --> 
+        <nav class="min-nav">
+            <ul>
+                <li><a href="#"> Home </a></li>
+                <i class="fas fa-chevron-right"></i>
+                <li><a class="current" href="categories.html"> Categories </a></li>
+            </ul>
+        </nav>   
+
+    <!-- Story Categories -->
+    <span >
+        <h1 class="container span"> All Story Categories </h1>
+    </span>    
+
+    <div class="wrapper">     
+        <div class="item"><a href="#"> <img src="img/Myth.jpg"> <div class="info"> Myths </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fairytales.JPG"> <div class="info"> Fairytales </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/animals.jpeg"> <div class="info"> Animals </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fearful2.png"> <div class="info"> Fearful </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fables.jpeg"> <div class="info"> Fables </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/history.png"> <div class="info"> History </div> </a></div>         
+    </div>
+
+    <!-- Poem Categories-->
+    <span >
+            <h1 class="container span"> Poems </h1>
+        </span>    
+    
+    <div class="wrapper">     
+        <div class="item"><a href="#"> <img src="img/Myth.jpg"> <div class="info"> Animal Poem </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fairytales.JPG"> <div class="info"> Adventure </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/animals.jpeg"> <div class="info"> Lullaby </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fearful2.png"> <div class="info"> Fearful </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/fables.jpeg"> <div class="info"> Fables </div> </a></div>
+        <div class="item"><a href="#"> <img src="img/history.png"> <div class="info"> History </div> </a></div>         
+    </div>
+
+    <!-- Footer goes here -->
+
+
+</body>
+</html>
