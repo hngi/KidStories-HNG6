988bd49d684b625a4ac7bd9043b2c92ee73f6b75
EphraimDev <aigbefoephraim@yahoo.com>
2019-04-30 09:54:08 +0100

    Fix landing page

--- public/js/index.js
+++ public/js/index.js
@@ -1,143 +1,23 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
-/******/ })
-/************************************************************************/
-/******/ ({
+var react = async function(event) {
+    event.preventDefault();
+    var text = event.target.textContent;
+    //console.log(text)
+    var storyId = event.target.dataset.storyId;
+    var token = $('.hidden').html;
+    console.log(token);
+    if (text === "Like") {
+        console.log(1)
+        const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/like', {
+            action: text,
+            header: {
 
-/***/ "./resources/js/index.js":
-/*!*******************************!*\
-  !*** ./resources/js/index.js ***!
-  \*******************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+            }
+        });
+        console.log(action)
+    } else {
+        const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/dislike', {
+            action: text
+        });
+    }
 
-// var updateStoryStats = {
-//     Like: function (storyId) {
-//         document.querySelector('#likes-count-' + storyId).textContent++;
-//     },
-//     Unlike: function(storyId) {
-//         document.querySelector('#likes-count-' + storyId).textContent--;
-//     }
-// };
-// var toggleButtonText = {
-//     Like: function(button) {
-//         button.textContent = "Unlike";
-//     },
-//     Unlike: function(button) {
-//         button.textContent = "Like";
-//     }
-// };
-var react = function react(event) {
-  console.log(event); // var storyId = event.target.dataset.storyId;
-  // var action = event.target.textContent;
-  // toggleButtonText[action](event.target);
-  // updateStoryStats[action](storyId);
-  // axios.post('/stories/' + storyId + '/reactions/like',
-  //     { action: action });
-}; // Echo.channel('KidsStories')
-//             .listen('Reaction', function(event) {
-//                 console.log(event);
-//                 var action = event.action;
-//                 updateStoryStats[action](event.storyId);
-//             })
-// $('.like').on('click', function(){
-//     console.log(event)
-// })
-
-/***/ }),
-
-/***/ 2:
-/*!*************************************!*\
-  !*** multi ./resources/js/index.js ***!
-  \*************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! C:\Users\EPHRAIM\Desktop\HNG\kidstories\resources\js\index.js */"./resources/js/index.js");
-
-
-/***/ })
-
-/******/ });
+};
