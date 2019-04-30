f340a5458e1b732d8688f2a896f35ce825759038
EphraimDev <aigbefoephraim@yahoo.com>
2019-04-30 11:47:49 +0100

    Add footer to app.blade

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,23 @@
 var react = async function(event) {
-    event.preventDefault();
-    var text = event.target.textContent;
-    //console.log(text)
-    var storyId = event.target.dataset.storyId;
-    var token = $('.hidden').html;
-    console.log(token);
-    if (text === "Like") {
-        console.log(1)
-        const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/like', {
-            action: text,
-            header: {
+  event.preventDefault();
+  var text = event.target.textContent;
+  //console.log(text)
+  var storyId = event.target.dataset.storyId;
+  var token = $('.hidden').html;
+  console.log(token);
+  if (text === "Like") {
+      console.log(1)
+      const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/like', {
+          action: text,
+          header: {
 
-            }
-        });
-        console.log(action)
-    } else {
-        const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/dislike', {
-            action: text
-        });
-    }
+          }
+      });
+      console.log(action)
+  } else {
+      const action = await axios.post('/api/v1/stories/' + storyId + '/reactions/dislike', {
+          action: text
+      });
+  }
 
 };
