89ee790ac02b645b28d600a89b79f2a8cd1d2b0b
kevchike <kevchike@gmail.com>
2019-05-05 17:00:24 +0100

    Code review: Category module for user

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,42 +24,7 @@ class CategoryController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Request $request, $id)
-    {
-        //
-        $category = Category::find($id);
-        $user = $request->user();
-
-
-        for ($i=0; $i < $category->stories->count(); $i++) {
-            $storyId = $category->stories[$i]->id;
-            if ($user) {
-                $reaction = Reaction::where('story_id', $storyId)
-                ->where('user_id', $user->id)
-                ->first();
-                if ($reaction && $reaction->reaction == 0) {
-                    $category->stories[$i]['reaction'] = 'dislike';
-                } elseif ($reaction && $reaction->reaction == 1) {
-                    $category->stories[$i]['reaction'] = 'like';
-                } else {
-                    $category->stories[$i]['reaction'] = 'nil';
-                }
-            }else {
-                $category->stories[$i]['reaction'] = 'nil';
-            }
-
-        }
-        return view('storieslisting', compact('category'));
-
-    }
-
-    /**
-     * Display the specified resource.
+     * Display the stories related to a category.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -76,24 +41,13 @@ class CategoryController extends Controller
             $stories = Story::where('category_id', $id);
         }
 
-        $stories = ($request->query('sort') == 'latest') 
-                        ? $stories->latest()->get() 
-                        : $stories->get();
-
-        //Retrieve current query strings:
-        $currentQueries = $request->query();
-
-        //Declare new queries you want to append to string:
-        $newQueries = [$request->query('search'), $request->query('sort')];
-
-        //Merge together current and new query strings:
-        $allQueries = array_merge($currentQueries, $newQueries);
-
-        //Generate the URL with all the queries:
-        $request->fullUrlWithQuery($allQueries);
-
-        // dd($request->fullUrlWithQuery($allQueries));
-        // dd($allQueries);
+        if ($request->query('sort') == 'latest') {
+            $stories = $stories->latest()->paginate(21);
+        } else if ($request->query('sort') == 'age') {
+            $stories = $stories->orderBy("age_from")->paginate(21);
+        } else {
+            $stories = $stories->paginate(21);
+        }
 
         $categories = Category::limit(4)->get();
 
@@ -132,70 +86,6 @@ class CategoryController extends Controller
 
         return view('categories_stories', compact('stories', 'categories', 'currentCategory'));
 
-    }
-
-    /**
-     *
-     */
-    public function filter(Request $request, $id)
-    {
-        $stories = Story::orderBy('id','desc')->where('category_id',$id)->get();
-        $category = Category::find($id);$user = $request->user();
-
-        $user = $request->user();
-
-
-        for ($i=0; $i < $stories->count(); $i++) {
-            $storyId = $stories[$i]->id;
-            if ($user) {
-                $test = 0;
-                $reaction = Reaction::where('story_id', $storyId)
-                ->where('user_id', $user->id)
-                ->first();
-                if ($reaction && $reaction->reaction == 0) {
-                    $stories[$i]['reaction'] = 'dislike';
-                } elseif ($reaction && $reaction->reaction == 1) {
-                    $stories[$i]['reaction'] = 'like';
-                } else {
-                    $stories[$i]['reaction'] = 'nil';
-                }
-            }else {
-                $test = 1;
-                $stories[$i]['reaction'] = 'nil';
-            }
-
-        }
-        return view('storieslisting', compact('category','stories'));
-    }
-    public function filterByAge(Request $request, $id)
-    {
-        $stories = Story::orderBy('age_from','desc')->where('category_id', $id)->get();
-        $category = Category::find($id);
-        $user = $request->user();
-
-
-        for ($i=0; $i < $stories->count(); $i++) {
-            $storyId = $stories[$i]->id;
-            if ($user) {
-                $test = 0;
-                $reaction = Reaction::where('story_id', $storyId)
-                ->where('user_id', $user->id)
-                ->first();
-                if ($reaction && $reaction->reaction == 0) {
-                    $stories[$i]['reaction'] = 'dislike';
-                } elseif ($reaction && $reaction->reaction == 1) {
-                    $stories[$i]['reaction'] = 'like';
-                } else {
-                    $stories[$i]['reaction'] = 'nil';
-                }
-            }else {
-                $test = 1;
-                $stories[$i]['reaction'] = 'nil';
-            }
-
-        }
-
-        return view('storieslisting', compact('category','stories'));
     }
 }
 
