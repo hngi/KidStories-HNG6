1809dfbdf297d79a5b6301c15cc0226bdc62cd2b
Ebuka Abraham <buka4rill@gmail.com>
2019-05-05 13:06:02 +0100

    New single stories page by buka4rill

---
+++ singlestories_new/singlestory.php
@@ -0,0 +1,251 @@
+<?php include 'partials/header.php'; ?>
+
+<link rel="stylesheet" type="text/css" media="screen" href="singlecss/singlestory.css">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    
+<body>
+    <div class="page-wrapper">
+        <!-- Main Navigation-->
+            <?php include 'partials/navbar.php'; ?>
+        <!--End Main Navigation -->
+
+
+        <div class="content">
+            <!-- Breadcrumb --> 
+            <nav class="min-nav">
+                <ul>
+                    <li><a href="#"> Home </a></li>
+                    <i class="fa fa-chevron-right"></i>
+                    <li><a class="current" href="#"> The Dancing Girl </a></li>
+                </ul>
+            </nav>
+
+            <!-- Content begins -->
+            <span class="content1 topic">
+                <h1> The Dancing Girl </h1>
+                <h3> By: Diana Jones </h3>
+            </span>
+
+            <!-- Story section -->
+            <div class="content1">
+                <!-- Bookmark story -->
+                <div class="subContent">
+                    <div class="">
+                        <a>
+                            <i class="fa fa-bookmark stBookmark"></i>
+                        </a>
+                    </div> <!-- Bookmark story ends -->
+
+                    <!-- Stories -->
+                    <img class="stories" src="../singlecss/img/1.jpeg">             
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy text ever since the 1500s, "it has survived not only five 
+                    centuries. </p>
+                    <img class="stories" src="../singlecss/img/13.jpeg">             
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy text ever since the 1500s, "it has survived not only five 
+                    centuries. </p>
+                    <img class="stories" src="../singlecss/img/7.jpeg">             
+                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy text ever since the 1500s, "it has survived not only five 
+                    centuries. </p>
+                    <img class="stories" src="../singlecss/img/16.jpeg">             
+                    <p class="pLast"> "Lorem Ipsum is simply dummy text of the printing and typesetting industry." </p>
+                    
+                </div>
+
+                <h1 class="end"> THE END </h1>
+                <!-- Story section ends -->
+
+                <!-- Tags -->
+                <div class="tags">
+                    <div>
+                        <button class="" type="submit" id="submit"> dragon </button>
+                        <button type="submit" id="submit"> history </button>
+                        <button type="submit" id="submit"> dragon </button>
+                        <button type="submit" id="submit"> history </button> 
+                        <i class="fa fa-thumbs-down thumbs"><span> 627 </span></i>
+                        <i class="fa fa-thumbs-up thumbs"><span> 3255 </span></i>
+                    </div>
+                </div>
+                <!-- Tags ends -->
+
+                <hr>
+                
+                <h1> Stories You Might Like </h1>
+
+                <!-- Cards section -->
+                <div class="stories py-5">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card story_card mt-4">
+                                <img src="../singlecss/img/20.jpeg" class="card-img-top cards" alt="story image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Tales of Snow White</h5>
+                                    <p class="card-text mb-1">by <span class="author">Peter Tarka</span></p>
+                                    <hr>
+                                    <p class="card-text">For kids 3-5 years</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <div class="reactions">
+                                        <a class="like" href="#">
+                                            <i class="fa fa-thumbs-up mr-2"></i>
+                                            <small class="mr-3"> 100 </small>
+                                        </a>
+                                        <a class="dislike" href="#">
+                                            <i class="fa fa-thumbs-down mr-2"></i>
+                                            <small> 200 </small>
+                                        </a>
+                                    </div>
+                                    <div class="bookmark">
+                                        <a>
+                                            <i class="fa fa-bookmark"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="card story_card mt-4">
+                                <img src="../singlecss/img/20.jpeg" class="card-img-top cards" alt="story image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Tales of Snow White</h5>
+                                    <p class="card-text mb-1">by <span class="author">Peter Tarka</span></p>
+                                    <hr>
+                                    <p class="card-text">For kids 3-5 years</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <div class="reactions">
+                                        <a class="like" href="#">
+                                            <i class="fa fa-thumbs-up mr-2"></i>
+                                            <small class="mr-3"> 100 </small>
+                                        </a>
+                                        <a class="dislike" href="#">
+                                            <i class="fa fa-thumbs-down mr-2"></i>
+                                            <small> 200 </small>
+                                        </a>
+                                    </div>
+                                    <div class="bookmark">
+                                        <a>
+                                            <i class="fa fa-bookmark"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="card story_card mt-4">
+                                <img src="../singlecss/img/20.jpeg" class="card-img-top cards" alt="story image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Tales of Snow White</h5>
+                                    <p class="card-text mb-1">by <span class="author">Peter Tarka</span></p>
+                                    <hr>
+                                    <p class="card-text">For kids 3-5 years</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <div class="reactions">
+                                        <a class="like" href="#">
+                                            <i class="fa fa-thumbs-up mr-2"></i>
+                                            <small class="mr-3"> 100 </small>
+                                        </a>
+                                        <a class="dislike" href="#">
+                                            <i class="fa fa-thumbs-down mr-2"></i>
+                                            <small> 200 </small>
+                                        </a>
+                                    </div>
+                                    <div class="bookmark">
+                                        <a>
+                                            <i class="fa fa-bookmark"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="card story_card mt-4">
+                                <img src="../singlecss/img/20.jpeg" class="card-img-top cards" alt="story image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Tales of Snow White</h5>
+                                    <p class="card-text mb-1">by <span class="author">Peter Tarka</span></p>
+                                    <hr>
+                                    <p class="card-text">For kids 3-5 years</p>
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <div class="reactions">
+                                        <a class="like" href="#">
+                                            <i class="fa fa-thumbs-up mr-2"></i>
+                                            <small class="mr-3"> 100 </small>
+                                        </a>
+                                        <a class="dislike" href="#">
+                                            <i class="fa fa-thumbs-down mr-2"></i>
+                                            <small> 200 </small>
+                                        </a>
+                                    </div>
+                                    <div class="bookmark">
+                                        <a>
+                                            <i class="fa fa-bookmark"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                                       
+                </div>    
+            </div> 
+        </div>        
+        
+        <!-- App Section -->
+        <section class="main-banner">
+    	    <div class="container2">
+			    <div class="row c">
+
+                    <!--Image Column-->
+                    <div class="col-lg-4 col-md-12 col-sm-12 ">
+                	    <img src="images/resources/bottom2.jpg" alt=""  />
+                    </div>
+
+            	
+                    <!--Content Column-->
+                    <div class="content-column col-lg-8 col-md-12 col-sm-12">
+                        <div class="applink">
+                            <h4>Get up close with your child</h4>
+                            <div class="text">The Kids Stories app is your go to app for free bedtime stories, fairy tales, poems and short stories for kids. Get in there and start reading!
+                            </div>
+					        <div class="buttons-box">
+				                <a href="#" class="theme-btn wow slideInLeft" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="images/icons/apple.png" alt="" /></a>
+				                <a href="#" class="theme-btn wow slideInRight" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="images/icons/playstore.png" alt="" /></a>
+			                </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section> 
+        <!-- App sections ends -->
+        <!-- Content ends -->
+    </div>
+
+        
+
+
+
+    <!-- Footer goes here -->
+    <?php include 'partials/footer.php'; ?>
+
+    <!--Scroll to top-->
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+    <!-- Scripts -->
+    <script src="tjs/paroller.js"></script>
+    <script src="tjs/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="./singleJS/single_script.js"></script>
+</body>
+
+
+
