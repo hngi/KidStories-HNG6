86e16ad2cb750073bf385994bdd6b85c02fc4af2
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2019-05-05 23:47:24 +0100

    fix bug on get stories

--- app/Http/Resources/StoryResource.php
+++ app/Http/Resources/StoryResource.php
@@ -42,8 +42,10 @@ class StoryResource extends JsonResource
             $reaction = Reaction::where('story_id', $storyId)
                     ->where('user_id', $user->id)
                     ->first();
-            if ($reaction) {
-                return $reaction->reaction; 
+            if ($reaction && $reaction->reaction == 0) {
+                return "disliked"; 
+            } elseif ($reaction && $reaction->reaction == 1) {
+                return "liked";
             }        
                    
         }
