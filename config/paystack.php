a9d1488a22e1e2ca3346c3d4e2a5f305b2ea21d8
kevchike <kevchike@gmail.com>
2019-05-06 14:21:05 +0100

    Test Paystack

--- config/paystack.php
+++ config/paystack.php
@@ -15,24 +15,28 @@ return [
      * Public Key From Paystack Dashboard
      *
      */
-    'publicKey' => getenv('PAYSTACK_PUBLIC_KEY'),
+    // 'publicKey' => getenv('PAYSTACK_PUBLIC_KEY'),
+    'publicKey' => 'pk_test_a8b91d997d858cc6d82a62955c5b8fa519fdb554',
 
     /**
      * Secret Key From Paystack Dashboard
      *
      */
-    'secretKey' => getenv('PAYSTACK_SECRET_KEY'),
+    // 'secretKey' => getenv('PAYSTACK_SECRET_KEY'),
+    'secretKey' => 'sk_test_b8698ba8e8e6b56dab8601089a040895a2fe6962',
 
     /**
      * Paystack Payment URL
      *
      */
-    'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL'),
+    // 'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL'),
+    'paymentUrl' => 'https://api.paystack.co',
 
     /**
      * Optional email address of the merchant
      *
      */
-    'merchantEmail' => getenv('MERCHANT_EMAIL'),
+    // 'merchantEmail' => getenv('MERCHANT_EMAIL'),
+    'merchantEmail' => 'emeritusprofessoranis@gmail.com',
 
 ];
