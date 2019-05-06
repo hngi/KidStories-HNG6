ada75a37d86c0b833d316adbe33541e8d40f8084
tuck183 <okorie.tochi@gmail.com>
2019-05-06 15:19:12 +0100

    New modifications based on audit

--- resources/views/stories.blade.php
+++ resources/views/stories.blade.php
@@ -35,7 +35,7 @@
                                 @endif
 
                                 <div class="card-body story-card-body">
-                                    <h5 class="card-title"><a href="{{route('story.show',$story->slug)}}">{{$story->title}}</a></h5>
+                                    <h5 class="card-title"><a href="{{route('story.show',['story'=>$story->slug])}}">{{$story->title}}</a></h5>
                                     <p class="card-text">By <a href="#">{{$story->author}}</a></p>
                                     <hr style="margin:0 -5px;">
                                     <p>For Kids {{ $story->age_from .' to '. $story->age_to }} years</p>
