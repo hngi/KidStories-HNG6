bec7f194bbbe7e5c8d0be13ecea84def4447df8f
kevchike <kevchike@gmail.com>
2019-05-07 03:41:17 +0100

    User profile

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
 Route::get('/subscribe', function () {
     return view('subscribe');
 })->name('subscribe')->middleware('auth');
