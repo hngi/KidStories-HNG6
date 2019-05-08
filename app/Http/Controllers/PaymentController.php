f606a9770325f0817bbc3095bbc9ea43f7bba42e
prof-anis <emeritusprofessoranis@gmail.com>
2019-05-08 14:17:14 +0100

    perfected the payment email notification

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Paystack;
 use App\Payment;
@@ -7,6 +9,10 @@ use App\User;
 use App\Subscribed;
 use App\Subscription;
 use Carbon\Carbon;
+use Auth;
+use Notification;
+use App\Notifications\PaymentRecieved;
+
 class PaymentController extends Controller
 {
     //
@@ -18,6 +24,7 @@ class PaymentController extends Controller
     {
         return Paystack::getAuthorizationUrl()->redirectNow();
     }
+
     /**
      * Obtain Paystack payment information
      * @return void
@@ -25,16 +32,24 @@ class PaymentController extends Controller
     public function handleGatewayCallback()
     {
         $paymentDetails = Paystack::getPaymentData();
+
+
         $user=User::where('email',$paymentDetails['data']['customer']['email'])->first();
-    return    $this->updateDatabase($user,$paymentDetails['data']);
-        // Now you have the payment details,
-        // you can store the authorization_code in your db to allow for recurrent subscriptions
-        // you can then redirect or do whatever you want
+
+        // inform user that his payment has been recieved
+
+        Notification::send(Auth::user(),new PaymentRecieved($paymentDetails,$user));
+
+        return    $this->updateDatabase($user,$paymentDetails['data']);
+
+    
     }
+
     public function updateDatabase($user,$data)
     {
         //update subscribed table
         $subscription=Subscription::where('title',$data['metadata']['subscription'])->first();
+
         $today=Carbon::now();
         Subscribed::create([
             "user_id"=>$user->id,
@@ -42,13 +57,20 @@ class PaymentController extends Controller
             "expired_date"=>$today->addDays($subscription->duration),
             
         ]);
+
     
+
         //updating the payment table so we can also track users payment
+
         Payment::create([
             "user_id"=>$user->id,
             "transaction_reference"=>$data['reference'],
             "amount"=>$data['amount']
+
         ]);
+
         return redirect()->route('home');
+
+
     }
 }
