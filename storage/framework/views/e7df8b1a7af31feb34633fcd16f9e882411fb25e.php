460e0c0e7c1f5a8b27fed09ae5430e87d21155b9
kevchike <kevchike@gmail.com>
2019-05-10 14:59:17 +0100

    Prep

---
+++ storage/framework/views/e7df8b1a7af31feb34633fcd16f9e882411fb25e.php
@@ -0,0 +1,133 @@
+<nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+    <div class="container-fluid">
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Brand -->
+        <a class="navbar-brand pt-0" href="<?php echo e(route('admin.dashboard')); ?>">
+            
+            <h2 style="color: #6C66E5;">KIDSTORIES</h2>
+        </a>
+        <!-- User -->
+        <ul class="nav align-items-center d-md-none">
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="media align-items-center">
+                        <span class="avatar avatar-sm rounded-circle">
+                        <img alt="Image placeholder" src="<?php echo e(asset('argon')); ?>/img/icons/common/profile-icon.png">
+                        </span>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+                    <div class=" dropdown-header noti-title">
+                        <h6 class="text-overflow m-0"><?php echo e(__('Welcome!')); ?></h6>
+                    </div>
+                    <a href="#" class="dropdown-item">
+                        <i class="ni ni-single-02"></i>
+                        <span><?php echo e(__('My profile')); ?></span>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <i class="ni ni-settings-gear-65"></i>
+                        <span><?php echo e(__('Settings')); ?></span>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <i class="ni ni-calendar-grid-58"></i>
+                        <span><?php echo e(__('Activity')); ?></span>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <i class="ni ni-support-16"></i>
+                        <span><?php echo e(__('Support')); ?></span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="<?php echo e(route('admin.logout')); ?>" class="dropdown-item" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                        <i class="ni ni-user-run"></i>
+                        <span><?php echo e(__('Logout')); ?></span>
+                    </a>
+                </div>
+            </li>
+        </ul>
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+            <!-- Collapse header -->
+            <div class="navbar-collapse-header d-md-none">
+                <div class="row">
+                    <div class="col-6 collapse-brand">
+                        <a href="<?php echo e(route('admin.dashboard')); ?>">
+                            <img src="<?php echo e(asset('argon')); ?>/img/brand/blue.png">
+                        </a>
+                    </div>
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- Form -->
+            <form class="mt-4 mb-3 d-md-none">
+                <div class="input-group input-group-rounded input-group-merge">
+                    <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="<?php echo e(__('Search')); ?>" aria-label="Search">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <span class="fa fa-search"></span>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <!-- Navigation -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('admin.dashboard')); ?>">
+                        <i class="ni ni-tv-2 text-primary"></i> <?php echo e(__('Dashboard')); ?>
+
+                    </a>
+                </li>
+                
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('admin.user.index')); ?>">
+                        <i class="ni ni-circle-08 text-pink"></i> <?php echo e(__('Manage Users')); ?>
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('admin.categories.index')); ?>">
+                        <i class="ni ni-ungroup text-blue"></i> <?php echo e(__('Manage Categories')); ?>
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('admin.stories.index')); ?>">
+                        <i class="ni ni-planet text-blue"></i> <?php echo e(__('Manage Stories')); ?>
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('payment.index')); ?>">
+                        <i class="ni ni-money-coins text-blue"></i> <?php echo e(__('Payments')); ?>
+
+                    </a>
+                </li>
+
+
+                
+                
+
+
+                
+            </ul>
+            <!-- Divider -->
+            <hr class="my-3">
+            <!-- Heading -->
+            
+            <!-- Navigation -->
+            
+        </div>
+    </div>
+</nav><?php /**PATH /home/kelvin/HNG-5/CODE-TESTS/kidstories-main-repo/resources/views/admin/layouts/navbars/sidebar.blade.php ENDPATH**/ ?>
