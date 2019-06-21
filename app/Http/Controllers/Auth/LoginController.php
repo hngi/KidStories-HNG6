4361aa18debdbfa01fe7958cb9e93ecc4605fbb6
Chinedu Ebinim <chinedu@verifi.ng>
2019-06-21 15:45:17 +0100

    work on redirect after auth, and add padding to the footer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/stories';
 
     /**
      * Create a new controller instance.
