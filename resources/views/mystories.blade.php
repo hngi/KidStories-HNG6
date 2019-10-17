74bd2ee365b21f3bbbc8479fe0eaf9c5d93587a7
dokoede <dennisokoede@outlook.com>
2019-10-17 09:47:45 +0100

    bug fix for search-stories-section

--- resources/views/mystories.blade.php
+++ resources/views/mystories.blade.php
@@ -26,54 +26,54 @@
                 <div class="d-flex flex-row flex-wrap">
                     @forelse ($stories as $story)
                     <div class="col-lg-4 ">
-                    <div class="card col-lg-12 p-0 story-card mb-4 premium-badge-holder">
-                                @if($story->is_premium)
-                                    <span class="badge badge-primary premium-badge">PREMIUM</span>
-                                @endif
+                        <div class="card col-lg-12 p-0 story-card mb-4 premium-badge-holder">
+                            @if($story->is_premium)
+                            <span class="badge badge-primary premium-badge">PREMIUM</span>
+                            @endif
 
-                                @if($story->image_url )
-                                    <img src="{{ $story->image_url }}" />
-                                @else
-                                <img src="/images/placeholder.png" />
-                                @endif
+                            @if($story->image_url )
+                            <img src="{{ $story->image_url }}" />
+                            @else
+                            <img src="/images/placeholder.png" />
+                            @endif
 
-                                <div class="card-body story-card-body">
-                                    <h5 class="card-title"><a href="{{route('story.show',$story->slug)}}">{{$story->title}}</a></h5>
-                                    <p class="card-text">By <a href="{{route('author.stories', $story->author)}}">{{$story->author}}</a></p>
-                                    <hr style="margin:0 -5px;">
-                                    <p>For Kids {{ $story->age_from .' to '. $story->age_to }} years</p>
-                                    <hr style="margin:0 -20px;">
-                                    <div class="d-flex justify-content-between align-items-center card-">
-                                        <div class="btn-group">
-                                            @if ($story->reaction == 'dislike')
-                                            <i class="fas fa-thumbs-up fav-icon" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
-                                            <i class="fas fa-thumbs-down fav-icon fav-red" id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
-                                            @elseif ($story->reaction == 'like')
-                                            <i class="fas fa-thumbs-up fav-icon fav-green" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
-                                            <i class="fas fa-thumbs-down fav-icon " id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
-                                            @else
-                                            <i class="fas fa-thumbs-up fav-icon" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
-                                            <i class="fas fa-thumbs-down fav-icon" id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
-                                            @endif
-                                        </div>
-                                        <span class="verticalLine">
-                                        @if ($story->favorite == true)
-                                            <a> <i class="far fa-bookmark bookmark-blue" style="margin-left: 8px" onclick="bookmark(event);" id="bookmark-{{ $story->id }}" data-story-id="{{ $story->id }}"></i> </a>
-                                            @else
-                                            <a> <i class="far fa-bookmark" style="margin-left: 8px" onclick="bookmark(event);" id="bookmark-{{ $story->id }}" data-story-id="{{ $story->id }}"></i> </a>
-                                            @endif
-                                        </span>
+                            <div class="card-body story-card-body">
+                                <h5 class="card-title"><a href="{{route('story.show',$story->slug)}}">{{$story->title}}</a></h5>
+                                <p class="card-text">By <a href="{{route('author.stories', $story->author)}}">{{$story->author}}</a></p>
+                                <hr style="margin:0 -5px;">
+                                <p>For Kids {{ $story->age_from .' to '. $story->age_to }} years</p>
+                                <hr style="margin:0 -20px;">
+                                <div class="d-flex justify-content-between align-items-center card-">
+                                    <div class="btn-group">
+                                        @if ($story->reaction == 'dislike')
+                                        <i class="fas fa-thumbs-up fav-icon" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
+                                        <i class="fas fa-thumbs-down fav-icon fav-red" id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
+                                        @elseif ($story->reaction == 'like')
+                                        <i class="fas fa-thumbs-up fav-icon fav-green" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
+                                        <i class="fas fa-thumbs-down fav-icon " id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
+                                        @else
+                                        <i class="fas fa-thumbs-up fav-icon" style="margin-right:8px;margin-top:6px;" id="fav-like-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}"></i><small class="mr-3" id="likes-count-{{ $story->id }}">{{$story->likes_count}}</small>
+                                        <i class="fas fa-thumbs-down fav-icon" id="fav-dislike-{{ $story->id }}" onclick="react(event);" data-story-id="{{ $story->id }}" style="margin-top:10px; margin-right:10px;margin-left:10px;"></i><small id="dislikes-count-{{ $story->id }}">{{$story->dislikes_count}}</small>
+                                        @endif
                                     </div>
+                                    <span class="verticalLine">
+                                        @if ($story->favorite == true)
+                                        <a> <i class="far fa-bookmark bookmark-blue" style="margin-left: 8px" onclick="bookmark(event);" id="bookmark-{{ $story->id }}" data-story-id="{{ $story->id }}"></i> </a>
+                                        @else
+                                        <a> <i class="far fa-bookmark" style="margin-left: 8px" onclick="bookmark(event);" id="bookmark-{{ $story->id }}" data-story-id="{{ $story->id }}"></i> </a>
+                                        @endif
+                                    </span>
                                 </div>
                             </div>
                         </div>
+                    </div>
                     @empty
-                        <p style="font-size:24px; margin-top: 20px; font-weight: 200; text-align: center;">
-                            Oops! You don't have any story yet.
-                            <a href="{{ route('story.create') }}" class="btn btn-block" style="margin-top: 10px;">
-                                Create New Story
-                            </a>
-                        </p>
+                    <p style="font-size:24px; margin-top: 20px; font-weight: 200; text-align: center;">
+                        Oops! You don't have any story yet.
+                        <a href="{{ route('story.create') }}" class="btn btn-block" style="margin-top: 10px;">
+                            Create New Story
+                        </a>
+                    </p>
                     @endforelse
                 </div>
 
@@ -82,14 +82,14 @@
                 </div>
             </div>
             @if ($stories->count())
-                <p style="font-size:24px; margin-top: 20px; font-weight: 200; text-align: center;">
-                    <a href="{{ route('story.create') }}" class="btn btn-block" style="margin-top: 10px;">
-                        Create New Story
-                    </a>
-                </p>
+            <p style="font-size:24px; margin-top: 20px; font-weight: 200; text-align: center;">
+                <a href="{{ route('story.create') }}" class="btn btn-block" style="margin-top: 10px;">
+                    Create New Story
+                </a>
+            </p>
             @endif
         </div>
-        <div class="col-md-5 col-lg-3 col-sm-6 catcs">
+        <div class="col-md-3 col-lg-3  catcs">
             <div class="d-flex flex-row col-md-12  ">
                 <div class="col-md-12" id="category-drop">
                     <p>Search My Stories</p>
@@ -97,7 +97,7 @@
                         <a href="{{ route('categories.stories', $category->id) }}">{{ $category->name }}</a><br>
                     @endforeach--}}
 
-                    <hr style="width:10%;"> 
+                    <hr style="width:10%;">
                     <div class="searchContainer">
                         <i class="fa fa-search searchIcon"></i>
                         <form action="{{ url()->current() }}">
@@ -106,7 +106,7 @@
                     </div>
                     <hr style="width:10%;">
                     <p>Sort By</p>
-                    <div class="card" style="width: 15rem;">
+                    <div class="card" style="width: 100%;">
                         <form action="{{ url()->current() }}" method="GET">
                             <input type="hidden" name="search" value="{{ request()->query('search') }}">
                             <ul class="list-group list-group-flush">
@@ -114,16 +114,14 @@
                                     <small style="display: block;">Max Age:</small>
                                     <select class="form-control form-control-sm" name="minAge">
                                         <option value="">Any age</option>
-                                        @for ($i = 0; $i < 18; $i++)
-                                            <option value="{{ $i }}" {{ !is_null(request()->query('minAge')) && request()->query('minAge') == $i ? 'selected' : '' }}>{{ $i }}</option>
-                                        @endfor
+                                        @for ($i = 0; $i < 18; $i++) <option value="{{ $i }}" {{ !is_null(request()->query('minAge')) && request()->query('minAge') == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                            @endfor
                                     </select>
                                     <small style="margin-top: 8px;display: block;">Min Age:</small>
                                     <select class="form-control form-control-sm" name="maxAge" style="margin-bottom: 8px;">
                                         <option value="">Any age</option>
-                                        @for ($i = 1; $i < 18; $i++)
-                                            <option value="{{ $i }}" {{ !is_null(request()->query('minAge')) && request()->query('maxAge') == $i ? 'selected' : '' }}>{{ $i }}</option>
-                                        @endfor
+                                        @for ($i = 1; $i < 18; $i++) <option value="{{ $i }}" {{ !is_null(request()->query('minAge')) && request()->query('maxAge') == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                            @endfor
                                     </select>
                                 </li>
                                 <li class="list-group-item">
@@ -131,10 +129,10 @@
                                     <select class="form-control form-control-sm" name="category" style="margin-bottom: 8px;">
                                         <option value="">All Category</option>
                                         @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}" {{ !is_null(request()->query('category')) && request()->query('category') == $category->id ? 'selected' : '' }}>{{ $category->name }}</option>
+                                        <option value="{{ $category->id }}" {{ !is_null(request()->query('category')) && request()->query('category') == $category->id ? 'selected' : '' }}>{{ $category->name }}</option>
                                         @endforeach
                                     </select>
-                                </li> 
+                                </li>
                                 {{-- <li class="list-group-item"><a href="{{ url()->current(). '?search=' . request()->query('search') . '&sort=latest' }}" style="color:inherit;">Most Recent </a><i class="fas fa-tint icon-right"></i></li> --}}
 
                                 <li class="list-group-item">
@@ -168,7 +166,7 @@
                     <div class="text">The Kids Stories app is your go to app for free bedtime stories, fairy tales, poems and short stories for kids. Get in there and start reading!
                     </div>
                     <div class="buttons-box">
-                     <!--    <a href="#" class="theme-btn wow slideInLeft" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="{{ asset('images/icons/apple.png') }}" alt="" /></a> -->
+                        <!--    <a href="#" class="theme-btn wow slideInLeft" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="{{ asset('images/icons/apple.png') }}" alt="" /></a> -->
                         <a href="https://github.com/hnginternship5/kidstories-android/blob/production/Bedtimestory/app/debug/app-debug.apk" class="theme-btn wow slideInRight" data-wow-delay="0ms" data-wow-duration="1500ms"><img src="{{ asset('images/icons/playstore.png') }}" alt="" /></a>
                     </div>
                 </div>
