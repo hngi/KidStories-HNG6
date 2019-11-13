3397a3298e8d1923d02e5a859072f89d4cee4106
Makabi Yelmis <gyelmis@gmail.com>
2019-11-13 21:36:28 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@kidstories.app'),
         'name' => env('MAIL_FROM_NAME', 'Kidstories'),
     ],
 
